2cc5fcf3cc8caa5e6c09e90c11e35f4b963cd105
thekim123 <tacdo12@naver.com>
2023-01-15 20:13:38 +0900

    SvgPicture 라이브러리 사용

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,14 +1,17 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.yellow,
+      body: ListView(
+        children: [
+          SvgPicture.asset("assets/logo.svg", height: 70, width: 70,)
+        ],
       ),
     );
   }
