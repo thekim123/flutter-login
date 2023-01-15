0e2e2de0083db2c6508d98df103bd928f48b35fa
thekim123 <tacdo12@naver.com>
2023-01-15 20:02:10 +0900

    앱 구조 및 뼈대 만들기

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.yellow,
+      ),
+    );
+  }
+}
